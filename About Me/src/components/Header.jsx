999f32fc6a18ca4140eb26fec0351b717a56cf70
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-05-05 14:51:50 +1000

    Added Image
    Redisgning App.jsx and adding image to header

---
+++ About Me/src/components/Header.jsx
@@ -0,0 +1,13 @@
+//Header
+
+export default function Header() {
+    return (
+        <header>
+            <div className="headerDiv-text">
+                <img src="./images/Pikachu.jpg" alt = "">
+                <h1>MahabaHubba</h1>
+                </img>
+            </div>
+        </header>
+    )
+}
