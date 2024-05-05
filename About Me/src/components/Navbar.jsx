07077f969b8ded501af59701caa924d324ee7c17
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-05-05 17:10:11 +1000

    Pages have now started to link
    There were errors oin the router in which there was no content from about mew and all other concepts in the navbar folder

--- About Me/src/components/Navbar.jsx
+++ About Me/src/components/Navbar.jsx
@@ -4,19 +4,6 @@ import {Link} from 'react-router-dom'
 
 export default function NavOptions () {
     return (
-        <Navbar
-        link = {[
-            <Link key={1} className = "nav-link text-light" to='/' >
-            About Me
-            </Link>,
-             <Link key={2} className = "nav-link text-light" to='/portfolio' >
-            Portfolio
-            </Link>,
-             <Link key={3} className = "nav-link text-light" to='/contact' >
-             Contact Me
-             </Link>
-        ]}
-        >
-        </Navbar>
+       <h1> Hello </h1>
     )
 }
