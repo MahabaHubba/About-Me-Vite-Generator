fac46460be134286ec2237c658cca15b346ee273
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-05-07 00:30:13 +1000

    Added Footer
    Added Foooter and changed direction of About me page to have it in rows instead of columns

--- About Me/src/components/Navbar-Pages/AboutMe.jsx
+++ About Me/src/components/Navbar-Pages/AboutMe.jsx
@@ -1,5 +1,6 @@
 import NavOptions from "../Navbar"
 import '../aboutme.css';
+import Footer from "../Footer";
 
 export default function AboutMe() {
 
@@ -8,21 +9,31 @@ export default function AboutMe() {
 
         <NavOptions/>
 
-        <div className="aboutme">
-        <div class="container mt-5 ">
-        <div class="d-flex">
-            <div className="boxborder" class="flex-grow-1 bg-light border mx-2">
-                <h5 class="text-center">About Me</h5>
-                Outside of coding, Im an avid powerlifter and a coffee addict 
-            </div>
-            <div className="boxborder" class="flex-grow-1 bg-light border ">
-                <h5 class="text-center"></h5>
-                I am an inspiring coder, who underwent a 6 month bootcamp to learn the fundamentals of full stack development.
-                I am proficient in both backend and front end development and continue to enjoy improving my arsenal of skills in the world of coding
-            </div>
+        <div class="aboutme">
+    <div class="container mt-5">
+        <div class="row justify-content-between">
+            
+                <div class="boxborder bg-light border mb-4">
+                    <h5 class="text-center">About Me</h5>
+                    Outside of coding, I'm an avid powerlifter and a coffee addict 
+                </div>
+             
+                <div class="boxborder bg-light border mb-4">
+                    <h5 class="text-center"></h5>
+                    I am an inspiring coder, who underwent a 6-month bootcamp to learn the fundamentals of full-stack development. I am proficient in both backend and front end development and continue to enjoy improving my arsenal of skills in the world of coding.
+
+                   <p>Future learning aspect:
+                    TypeScript, Svylette, Next.Js
+                   </p>
+                   
+                </div>
+            
+            
+            
         </div>
     </div>
-    </div>
+</div>
+    <Footer />
         
        </>
     )
