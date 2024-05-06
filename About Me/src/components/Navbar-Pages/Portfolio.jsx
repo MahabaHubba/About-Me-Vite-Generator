79bce7f9eb877e301487eada9beaae505e0ef7b8
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-05-07 00:42:12 +1000

    starting page for portfolio
    INstallation of navbar and footer

--- About Me/src/components/Navbar-Pages/Portfolio.jsx
+++ About Me/src/components/Navbar-Pages/Portfolio.jsx
@@ -1,9 +1,13 @@
 import NavOptions from "../Navbar"
+import Footer from "../Footer"
 export default function Portfolio() {
     return(
         <>
         <NavOptions/>
+        <div class="aboutme">
         <h1>hello welcome to Portfolio</h1>
+        </div>
+        <Footer />
        </>
     )
 }
