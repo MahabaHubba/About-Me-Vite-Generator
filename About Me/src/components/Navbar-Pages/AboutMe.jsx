aa2cef12e59393e4ff00159520dd80ec505855a1
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-05-06 23:19:28 +1000

    Added bootstrap to about me page
    Added border to the divs around around me and added bootstrap

--- About Me/src/components/Navbar-Pages/AboutMe.jsx
+++ About Me/src/components/Navbar-Pages/AboutMe.jsx
@@ -1,10 +1,29 @@
 import NavOptions from "../Navbar"
+import '../aboutme.css';
+
 export default function AboutMe() {
 
     return(
         <>
+
         <NavOptions/>
-        <h1>hello welcome to about Me</h1>
+
+        <div className="aboutme">
+        <div class="container mt-5 ">
+        <div class="d-flex">
+            <div className="boxborder" class="flex-grow-1 bg-light border mx-2">
+                <h5 class="text-center">About Me</h5>
+                Outside of coding, Im an avid powerlifter and a coffee addict 
+            </div>
+            <div className="boxborder" class="flex-grow-1 bg-light border ">
+                <h5 class="text-center"></h5>
+                I am an inspiring coder, who underwent a 6 month bootcamp to learn the fundamentals of full stack development.
+                I am proficient in both backend and front end development and continue to enjoy improving my arsenal of skills in the world of coding
+            </div>
+        </div>
+    </div>
+    </div>
+        
        </>
     )
 }
