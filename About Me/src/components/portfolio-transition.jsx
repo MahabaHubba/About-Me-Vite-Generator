8bb57902d3d1e3b81086a9d484a3bee555237f84
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-05-07 15:00:20 +1000

    Added  two options
    To change between teh project as they click the arrow

--- About Me/src/components/portfolio-transition.jsx
+++ About Me/src/components/portfolio-transition.jsx
@@ -1,2 +1,24 @@
 import { useState } from 'react';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+
+function portfolioTransition() {
+    const [showFirstProject, setShowFirstProject] = useState(true);
+
+    const toggleProject = () => {
+        setShowFirstProject(!showFirstProject)
+    };
+
+    return (
+        <div className='d-flex flex-column align-items-center ms-3'>
+            {showFirstProject ? (
+                <img src="./images/ichooseyou.png" alt="pokemon" className="mb-3 shadow" />
+            ):(<img src="./img/wagerless.png" alt="developer's pic" className="mb-3 shadow" />)}
+            <div className='d-flex'>
+                <button>
+
+                </button>
+
+            </div>
+        </div>
+    )
+}
