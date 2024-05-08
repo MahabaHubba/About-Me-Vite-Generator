e81661c81f040a75bf103048f6e61c3294b74d91
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-05-08 11:17:04 +1000

    Contact event
    prevent default as well

--- About Me/src/components/Navbar-Pages/Contact.jsx
+++ About Me/src/components/Navbar-Pages/Contact.jsx
@@ -1,13 +1,32 @@
 import NavOptions from "../Navbar"
 import Footer from '../Footer';
 import '../contact.css'
+import { useState } from 'react';
 
 export default function Contact() {
+
+    const [contactData, setContactData] = useState({
+        name: "",
+        email: "",
+        comment: "",
+    });
+
+    const contactEvent = (e) => {
+        const {name, value} = e.target
+        setContactData({
+            ...contactData,
+            [name]: value
+        })
+    };
+
+    const submitContact = (e) => {
+        e.preventDefault();
+    }
+
     return(
         <>
          <NavOptions/>
          <h1>hello welcome to my contact</h1>
-
 
          <Footer />
         </>
