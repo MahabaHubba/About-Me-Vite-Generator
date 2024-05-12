4958299fe15474fc30e342c728ff6f2c35452ad2
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-05-12 22:27:03 +1000

    debugging
    Attemot changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,7 +27,7 @@ export default function Footer() {
             <a href="https://www.linkedin.com/in/mohibul-haque-14782020b/" target="_blank"  title="LinkedIn" rel="noreferrer">
               <img src="linkedin.png" alt="linkedin logo" />
             </a>
-            <a href="./images/Md-Mohibul-Haque-Resume.pdf" target="_blank"  title="Resume" rel="noreferrer">
+            <a href="./public/Md-Mohibul-Haque-Resume.pdf" target="_blank"  title="Resume" rel="noreferrer">
               <img src="resume.png" alt="Resume logo" />
             </a>
           </div>
